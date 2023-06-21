2c874a1fd94784e7471d1879e48a2a3b7721652b
Dudhatkaran <karanvd2002@gmail.com>
2023-06-21 18:34:21 +0530

    start

---
+++ redux-main/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodolistContainers from './Containers/TodolistContainers';
+
+function App() {
+  return (
+    <>
+      <TodolistContainers />
+    </>
+  );
+}
+
+export default App;
